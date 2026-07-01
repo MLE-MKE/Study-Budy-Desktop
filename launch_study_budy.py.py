a3c1c20872183d529c7d57f7c6375b75629a0ee7
MLE-MKE <easy_mac55@ymail.com>
2026-06-30 20:46:29 -0500

    launcher

---
+++ launch_study_budy.py.py
@@ -0,0 +1,9 @@
+# ---- STUDY BUDY PROGRAM LAUNCHER ----
+
+# This imports the actual Study Budy startup function.
+from study_budy.main import main
+
+
+# This starts Study Budy when the packaged program is opened.
+if __name__ == __main__
+    raise SystemExit(main())
